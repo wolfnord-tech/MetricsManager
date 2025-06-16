c83661f2787e8302cae6be0ea9d28e55e0411455
WolfNord <131020058+wolfnord-tech@users.noreply.github.com>
2025-06-16 23:10:08 +0300

    Update metrics.h

--- data/metrics.h
+++ data/metrics.h
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-// Является объектом каждой метрики
+// Класс Metrics отвечает за объекты каждой метрики
 class Metrics {
     protected:
         string from;
